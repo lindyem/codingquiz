9058a025f39e105a197fb724498f766f64b6465f
lindyem <lindy.merwin@gmail.com>
2020-06-15 17:56:58 -0700

    Timer

--- script.js
+++ script.js
@@ -1,19 +1,79 @@
 var intro = document.querySelector("#intro");
 var startButton = document.querySelector("#startButton");
 var timer = document.querySelector("#timer");
+var questionTitle = document.querySelector("#questionTitle");
+var questionAnswers = document.querySelector("#questionAnswers");
+var questionStatus = document.querySelector('#questionStatus');
 var countdown = 75;
+var questionIndex = 0;
+
+var questions = [
+  {
+    title: "Commonly used data types do not include",
+    answers: [
+      {
+        text: "answer1",
+        isCorrect: true,
+      },
+      {
+        text: "answer2",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    title: "Second question",
+    answers: ["answer1", "answer2"],
+  },
+  {
+    title: "Third question",
+    answers: ["answer1", "answer2"],
+  },
+  {
+    title: "Fourth question",
+    answers: ["answer1", "answer2"],
+  },
+];
 
 function startQuiz() {
   intro.style.display = "none";
   setInterval(countdownTimer, 1000);
+  renderQuestion();
 }
 
 function countdownTimer() {
   timer.innerHTML = countdown;
   countdown--;
-
   // if countdown === 0
+}
+
+function renderQuestion() {
+  questionTitle.innerHTML = questions[questionIndex].title;
+
+  // for every item in answers make a button with the answer text
+  for (var i = 0; i < questions[questionIndex].answers.length; i++) {
+    var newLi = document.createElement("li");
+    var liBtn = document.createElement('button');
+    liBtn.innerHTML = (i + 1) + ". " + questions[questionIndex].answers[i].text;
+    liBtn.setAttribute('data-index', i);
+    newLi.append(liBtn);
+    questionAnswers.append(newLi);
+  }
+}
+
+function checkAnswer(event){
+  var answerIndex = event.target.getAttribute('data-index');
+  if(questions[questionIndex].answers[answerIndex].isCorrect) {
+    questionStatus.innerHTML = "Correct";
+  } else {
+    questionStatus.innerHTML = "Wrong";
+    countdown = countdown - 10;
+  }
+
+  questionIndex++;
+  renderQuestion();
 }
 
 // Event Listeners
 startButton.addEventListener("click", startQuiz);
+questionAnswers.addEventListener("click", checkAnswer)
